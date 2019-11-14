e12dffe3bd488ad4d600f9dc0d9229c801bb3268
blsonalib <blsonalib@gmail.com>
2019-11-14 13:53:06 +0530

    Use Case3

--- gamblerSimulator.sh
+++ gamblerSimulator.sh
@@ -2,13 +2,20 @@
 echo "Welcome To Gambler Simulator"
 stack=100
 BET=1
-result=$(( RANDOM%2 ))
-if [ $result -eq 0 ]
-then
-    stack=$(( $stack + 1 ))
-else
-    stack=$(( $stack - 1 ))
-fi
+percent=50
+goal=$stack
+numberOfGoals=$(( ($percent * 100) / 100 ))
+maxValue=$(( $stack + $numberOfGoals ))
+minValue=$(( $stack - $numberOfGoals ))
+while [ $maxValue -gt $goal ] && [ $minValue -lt $goal ]
+do
+   result=$(( RANDOM%2 )) 
+   if [ $result -eq 0 ]
+   then
+      goal=$(( $goal + 1 ))
+    else
+      goal=$(( $goal - 1 )) 
+     fi
+done 
 
-     
 
