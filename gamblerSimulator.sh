3659d660203c25e98355f35e6105bba199753d5d
blsonalib <blsonalib@gmail.com>
2019-11-14 16:43:06 +0530

    Use Case4

--- gamblerSimulator.sh
+++ gamblerSimulator.sh
@@ -1,21 +1,31 @@
 #!/bin/bash -x
 echo "Welcome To Gambler Simulator"
-stack=100
+declare -A storeAmount
+STAKE=100
 BET=1
-percent=50
-goal=$stack
-numberOfGoals=$(( ($percent * 100) / 100 ))
-maxValue=$(( $stack + $numberOfGoals ))
-minValue=$(( $stack - $numberOfGoals ))
-while [ $maxValue -gt $goal ] && [ $minValue -lt $goal ]
+PERCENT=50
+goal=$STAKE
+DAY=20
+numberOfGoals=$(( ($PERCENT * $STAKE) / 100 ))
+maxValue=$(( $STAKE + $numberOfGoals ))
+minValue=$(( $STAKE - $numberOfGoals ))
+for (( i=1; i<=$DAY; i++ ))
 do
-   result=$(( RANDOM%2 )) 
-   if [ $result -eq 0 ]
-   then
-      goal=$(( $goal + 1 ))
-    else
-      goal=$(( $goal - 1 )) 
-     fi
-done 
+   goal=$STAKE 	
+   while [ $maxValue -gt $goal ] && [ $minValue -lt $goal ]
+   do
+      result=$(( RANDOM%2 )) 
+      if [ $result -eq 0 ]
+      then
+         goal=$(( $goal + 1 ))
+         win=$(( $goal +  $STAKE ))
+         totalAmount=$(( $totalAmount + 1 ))
+      else
+         goal=$(( $goal - 1 )) 
+         loose=$(( $goal - $STAKE ))
+         totalAmount=$(( $totalAmount - 1))
+       fi
+      done
+done  
 
 
