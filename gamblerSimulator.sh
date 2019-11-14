ce800b9eeec4dbc090e7545e8bebd4de81d05002
blsonalib <blsonalib@gmail.com>
2019-11-14 12:01:27 +0530

    Use Case2

--- gamblerSimulator.sh
+++ gamblerSimulator.sh
@@ -1,5 +1,14 @@
 #!/bin/bash -x
 echo "Welcome To Gambler Simulator"
 stack=100
-bet=1
+BET=1
+result=$(( RANDOM%2 ))
+if [ $result -eq 0 ]
+then
+    stack=$(( $stack + 1 ))
+else
+    stack=$(( $stack - 1 ))
+fi
+
+     
 
