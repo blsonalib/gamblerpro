e9f9d2a41d7d7bb0f5a9dee56d9bea6d51c7d3ba
blsonalib <blsonalib@gmail.com>
2019-11-15 11:48:43 +0530

    Use case5

--- gamblerSimulator.sh
+++ gamblerSimulator.sh
@@ -1,16 +1,18 @@
 #!/bin/bash -x
 echo "Welcome To Gambler Simulator"
 declare -A storeAmount
+declare -A days
 STAKE=100
 BET=1
 PERCENT=50
 goal=$STAKE
 DAY=30
+counter=0
 numberOfGoals=$(( ($PERCENT * $STAKE) / 100 ))
 maxValue=$(( $STAKE + $numberOfGoals ))
 minValue=$(( $STAKE - $numberOfGoals ))
-for (( i=0; i<=$DAY; i++ ))
-do
+ for (( i=0; i<=$DAY; i++ ))
+ do
    goal=$STAKE 
    totalAmount=0	
    while [ $maxValue -gt $goal ] && [ $minValue -lt $goal ]
@@ -21,17 +23,21 @@ do
          goal=$(( $goal + $BET ))
          win=$(( $goal +  $STAKE ))
          totalAmount=$(($totalAmount + 1 ))
-        
+         counterDay=$(($counter+1))  
+         
       else
          goal=$(( $goal - $BET )) 
          loose=$(( $goal - $STAKE ))
          totalAmount=$(($totalAmount - 1 ))
-                 
+         counterDay=$((counter-1 ))        
        fi
       done
-done
+ done
 storeAmount["win"]=$win
-storeAmount["loose"]=$loose
 storeAmount["totalAmount"]=$totalAmount
-echo ${storeAmount[@]}  
-
+days=["counterDay"]=$counterDay
+echo ${days[@]}
+echo ${!storeAmount[@]}  
+echo ${storeAmount[@]}
+MaxValueWin=$( printf "%s\n" ${!storeAmount[@]} ${storeAmount[@]} | sort -nr | head -1  )
+minValueLoose=$( printf "%s\n" ${!storeAmount[@]} ${storeAmount[@]} | sort -nr | tail -1 )
